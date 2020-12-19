e413828c8f91f84ba89391163ebffbe06be3d923
Rudranks <70576278+Rudranks@users.noreply.github.com>
2020-12-20 00:54:19 +0400

    Delete YT.js

--- YT.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'YT',
-    description: 'Rick-roll for the unsuspecting ones',
-    execute(message, args) {
-
-            message.channel.send('<https://www.youtube.com/watch?v=ub82Xb1C8os>');
-    
-    }
-}
