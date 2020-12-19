4c6a35081177c1fb7a0fd13972e282ce2de0f6b9
Rudranks <70576278+Rudranks@users.noreply.github.com>
2020-12-20 00:53:53 +0400

    Delete Prefix.js

--- Prefix.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'Prefix',
-    description: 'Prints out prefix',
-    execute(message, args) {
- 
-            message.channel.send('The current prefix is %');
-    
-    }
-}
